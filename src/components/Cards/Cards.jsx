1a7a8298c5aa058fc5e69ab173d61b3c72ab4a2a
taissir alem <taissir.alem@gmail.com>
2024-12-24 13:23:43 +0100

    nos services

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,57 @@
+import "./Cards.css";
+import Team from "../../assets/slider/pic1.jpg";
+import Global from "../../assets/slider/pic2.webp";
+import Custom from "../../assets/slider/pic3.webp";
+
+import { useNavigate } from "react-router-dom";
+import ScrollAnimation from "react-animate-on-scroll";
+import { useTranslation } from "react-i18next"; // Importer le hook
+
+export default function Cards() {
+  const { i18n } = useTranslation(); // Utiliser useTranslation pour obtenir l'instance i18n
+  const navigate = useNavigate();
+
+  const products = [
+    {
+      id: 1,
+      titlefr: "Team Building",
+      titleen: "Team Building",
+      image: Team,
+      navigate: "/",
+    },
+    {
+      id: 2,
+      titlefr: "Accompagnement",
+      titleen: "Global Support",
+      image: Global,
+      navigate: "/",
+    },
+    {
+      id: 3,
+      titlefr: "Stands Personnalisés",
+      titleen: "Custom Stands",
+      image: Custom,
+      navigate: "/",
+    },
+  ];
+
+  // Mapping des produits avec le titre en fonction de la langue courante
+  const all = products.map((product) => {
+    const title = i18n.language === "fr" ? product.titlefr : product.titleen;
+
+    return (
+      <div key={product.id} className="cardsmain">
+        <div
+          className="cardscard"
+          style={{ backgroundImage: `url(${product.image})` }}
+        >
+          <h1 onClick={() => navigate(`/products${product.navigate}`)}>
+            {title}
+          </h1>
+        </div>
+      </div>
+    );
+  });
+
+  return <div className="allcardsmain">{all}</div>;
+}
--- src/components/NosServices/NosServices.jsx
+++ src/components/NosServices/NosServices.jsx
@@ -1,3 +1,19 @@
+import Cards from "../Cards/Cards";
+import "./NosServices.css";
+
 export default function NosServices() {
-  return <section id="services">NosServices</section>;
+  return (
+    <section id="services">
+      <div className="servicesheader">
+        <h1>Nos Services</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <Cards></Cards>
+    </section>
+  );
 }
